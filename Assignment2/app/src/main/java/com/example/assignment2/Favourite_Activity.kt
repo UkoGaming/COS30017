8a785ae9343921afbfd77dc4371e68dc3f5009be
UkoGaming <143693444+UkoGaming@users.noreply.github.com>
2026-03-13 12:13:58 +0700

    Add files via upload

---
+++ Assignment2/app/src/main/java/com/example/assignment2/Favourite_Activity.kt
@@ -0,0 +1,42 @@
+package com.example.assignment2
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class Favourite_Activity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var backBtn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.favourite)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.recyclerViewFavourite)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        recyclerView = findViewById(R.id.lsCars)
+        backBtn = findViewById(R.id.button_back)
+
+        val favouriteCars = GlobalState.cars
+            .filter { it.isFavourite }
+            .toMutableList()
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        recyclerView.adapter = FavouriteReViewAdapt(favouriteCars)
+
+        backBtn.setOnClickListener {
+            finish()
+        }
+    }
+}
